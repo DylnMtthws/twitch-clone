5e4cf02a1c9cd9503c487dc121742a5e96974aa8
dylanmatthews <djmatthews93@gmail.com>
2024-01-24 11:43:47 -0700

    navbar init

---
+++ app/(browse)/layout.tsx
@@ -0,0 +1,12 @@
+import { NavBar } from "./components/navbar";
+
+const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <NavBar />
+      <div className="flex h-full pt-20">{children}</div>
+    </>
+  );
+};
+
+export default BrowseLayout;
