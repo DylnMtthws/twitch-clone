8d148ba625cadb442e3e957351d4f91f41bd0cdd
Dylan Matthews <djmatthews93@gmail.com>
2024-03-01 10:51:16 -0500

    create sidebar

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,10 +1,14 @@
 import { NavBar } from "./components/navbar";
+import { Sidebar } from "./components/sidebar";
 
 const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <NavBar />
-      <div className="flex h-full pt-20">{children}</div>
+      <div className="flex h-full pt-20">
+        <Sidebar />
+        {children}
+      </div>
     </>
   );
 };
